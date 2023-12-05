081e9cf1e06d3f20cd62892da846163fe7707b17
Sudoer <sudoer.zero@gmail.com>
2023-12-05 21:09:33 +0300

    Add name as multiple pics.

---
+++ components/Name.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div class="flex space-x-4">
+      <div v-for="image in images" :key="image">
+        <img :src="`/Name/${image}`" class="-rotate-2" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+const images = ["o.png", "s.png", "a.png", "m.png", "a.png"];
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <Hero />
+    <Name />
   </div>
 </template>
 <script setup></script>
