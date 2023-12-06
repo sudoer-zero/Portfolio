f2586e5e0a32ee6a56d2afadaa482c1164bb65f9
Sudoer <sudoer.zero@gmail.com>
2023-12-06 17:44:12 +0300

    Add article card component.

---
+++ components/ArticleCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="w-full p-8 lg:p-4 mb-4 rounded-lg hover:bg-slate-100 hover:dark:bg-slate-800"
+  >
+    <nuxt-link to="/articles">
+      <span class="text-slate-500 text-sm">April 28, 2023</span>
+      <h1 class="text-slate-700 dark:text-slate-300 font-medium pt-2">
+        How to config tailwindcss in a nuxtjs project.
+      </h1>
+      <p class="text-slate-600 text-sm py-2">Sudoer Zero</p>
+      <nuxt-link to="/articles" class="text-teal-500 text-sm"
+        >Read More
+        <Icon name="uil:arrow-right" size="15" />
+      </nuxt-link>
+    </nuxt-link>
+  </div>
+</template>
