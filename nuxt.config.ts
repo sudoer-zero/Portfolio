c87d2c5ea4fe7a53332cfc4420478dbcb9068aec
Sudoer <sudoer.zero@gmail.com>
2023-12-07 22:07:32 +0300

    Config content code highlight.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxtjs/tailwindcss',
-    'nuxt-icon',
-  ],
-  devtools: { enabled: true }
-})
+  modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@nuxt/content"],
+  content: {
+    highlight: {
+      theme: "github-dark",
+      preload: [
+        "python",
+        "javascript",
+        "html",
+        "css",
+        "bash",
+        "json",
+        "markdown",
+        "yaml",
+        "vue",
+      ],
+    },
+  },
+  devtools: { enabled: true },
+});
